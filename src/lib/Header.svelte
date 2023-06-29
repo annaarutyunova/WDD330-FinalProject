d114040ab41512afd8268e188091db55c5651413
Anna Arutyunova <anna.arutyunova8848@gmail.com>
2023-06-29 15:30:10 -0600

    Add Main and Header component and some css

---
+++ src/lib/Header.svelte
@@ -0,0 +1,16 @@
+<script>
+    let screenSize;
+</script>
+<svelte:window bind:innerWidth={screenSize} />
+
+<div id="header">
+    {#if screenSize < 500}
+        <p id="logo">Memer</p>
+    {/if}
+    {#if screenSize >= 500}
+        <p id="logo">Memer</p>
+        <p id="signin">Sign In</p>
+    {/if}
+</div>
+
+
---
+++ src/lib/Main.svelte
@@ -0,0 +1,16 @@
+<script>
+    let screenSize;
+</script>
+<svelte:window bind:innerWidth={screenSize} />
+
+<h1 id="welcome">Welcome to the jungle of meme</h1>
+    {#if screenSize < 500}
+        <img id="welcomeMeme1" src="https://media.tenor.com/OeswMjtpFdQAAAAM/cat.gif" alt="cat smilling"/>
+        <a id="logoPhone" href="#" ><p>Sign In</p></a>
+    {/if}
+    {#if screenSize >= 500}
+        <div id="welcomeMemes">
+            <img id="welcomeMeme1" src="https://media.tenor.com/OeswMjtpFdQAAAAM/cat.gif" alt="cat smilling"/>
+            <img id=welcomeMeme2 src="https://media.tenor.com/fREyFxrehi0AAAAd/cat-lick-cat.gif" alt="cat lick">
+        </div>
+    {/if}
