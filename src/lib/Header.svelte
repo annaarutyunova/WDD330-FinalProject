812cb3cc9f7ed72d6154dee6e621d10a7cce8a29
Anna Arutyunova <anna.arutyunova8848@gmail.com>
2023-06-29 15:53:17 -0600

    Fix css in components

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,16 +1,39 @@
 <script>
-    let screenSize;
+
 </script>
-<svelte:window bind:innerWidth={screenSize} />
 
 <div id="header">
-    {#if screenSize < 500}
-        <p id="logo">Memer</p>
-    {/if}
-    {#if screenSize >= 500}
         <p id="logo">Memer</p>
         <p id="signin">Sign In</p>
-    {/if}
 </div>
+
+<style>
+    #header{
+    background-color: #2b2118;
+    color: white;
+    padding: 0 10px;
+    display: flex;
+    justify-content: space-between;
+    font-family: 'Indie Flower', cursive;
+}
+    #logo{
+        /* font-family: 'Indie Flower', cursive; */
+        font-size: 1.6em;
+        margin: 5px;
+    }
+    @media only screen and (min-width: 500){
+        #header{
+            background-color: #2b2118;
+            color: white;
+            padding: 0 10px;
+            display: flex;
+            justify-content: space-between;
+            font-family: 'Indie Flower', cursive;
+        }
+        #logo{
+            font-size: 1.6em;
+        }
+    }
+</style>
 
 
--- src/lib/Main.svelte
+++ src/lib/Main.svelte
@@ -1,16 +1,56 @@
 <script>
-    let screenSize;
 </script>
-<svelte:window bind:innerWidth={screenSize} />
 
-<h1 id="welcome">Welcome to the jungle of meme</h1>
-    {#if screenSize < 500}
-        <img id="welcomeMeme1" src="https://media.tenor.com/OeswMjtpFdQAAAAM/cat.gif" alt="cat smilling"/>
-        <a id="logoPhone" href="#" ><p>Sign In</p></a>
-    {/if}
-    {#if screenSize >= 500}
-        <div id="welcomeMemes">
-            <img id="welcomeMeme1" src="https://media.tenor.com/OeswMjtpFdQAAAAM/cat.gif" alt="cat smilling"/>
-            <img id=welcomeMeme2 src="https://media.tenor.com/fREyFxrehi0AAAAd/cat-lick-cat.gif" alt="cat lick">
-        </div>
-    {/if}
+<h1 id="welcome">Welcome to the jungle of meme</h1>       
+<div id="welcomeMemes">
+    <img id="welcomeMeme1" src="https://media.tenor.com/OeswMjtpFdQAAAAM/cat.gif" alt="cat smilling"/>
+    <img id=welcomeMeme2 src="https://media.tenor.com/fREyFxrehi0AAAAd/cat-lick-cat.gif" alt="cat lick">
+    <a id="logoPhone" href="#" ><p>Sign In</p></a>
+</div>
+
+<style>
+    #welcome{
+    width: fit-content;
+    text-align: center;
+    margin: 20px 30px;
+    }
+    #welcomeMeme1{
+        display: block;
+        width: 250px;
+        height:auto;
+        margin: 20px auto 0;
+    }
+    #welcomeMeme2{
+        display:none;
+    }
+    #logoPhone{
+        text-decoration: none;
+        display: block;
+        width: fit-content;
+        margin: 0 auto;
+    }
+    #logoPhone p{
+        text-align:center;
+        padding: 10px 20px;
+        border: 1px solid #2b2118;
+        color: black;
+        font-size: 1.6em;
+    }
+    @media only screen and (min-width: 500){
+        #welcomeMemes{
+            display:flex;
+            justify-content: space-around;
+        }
+        #welcomeMeme1, #welcomeMeme2{
+            display: line;
+            width: 250px;
+            height:250px;
+            margin: 20px auto 0;;
+            margin: 0;
+            padding: 0;
+        }
+    }
+    @media only screen and (min-width: 900){
+
+    }
+</style>
