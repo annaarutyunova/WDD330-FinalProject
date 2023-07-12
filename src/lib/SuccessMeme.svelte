849d8e29a862ec2bfa2d4b2130e4a088294dbcf0
Tyler242 <77693574+Tyler242@users.noreply.github.com>
2023-07-12 10:53:05 -0600

    meme creation complete - next is saving

---
+++ src/lib/SuccessMeme.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { onMount } from 'svelte';
+
+  export let createdMeme;
+
+  function init() {
+    console.log(createdMeme);
+  }
+
+  onMount(init);
+</script>
+
+<div id="created-meme-display">
+  <img src={createdMeme.url} alt="your created meme" />
+  <button type="button">Save</button>
+</div>
--- src/lib/TextMeme.svelte
+++ src/lib/TextMeme.svelte
@@ -7,11 +7,19 @@
   <img src={image.url} alt={image.name} />
   <div id="text-area">
     {#each Array(image.box_count) as _, index (index)}
-      <label for="text{index}">Caption {index}</label><br />
-      <input type="text" name="text{index}" id="text{index}" required />
+      <label for="text{index}">Caption {index + 1}</label><br />
+      <input
+        type="text"
+        name="text{index}"
+        id="text{index}"
+        required
+        maxlength="40"
+      />
       <br />
     {/each}
   </div>
+</div>
+<div id="control">
   <button type="button" on:click|preventDefault={prevSlide}>Back</button>
   <button type="submit">Submit</button>
 </div>
@@ -19,23 +27,24 @@
 <style>
   #text-meme-content {
     display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
     align-items: center;
     justify-items: center;
   }
   #text-meme-content img {
-    grid-column: 1/3;
+    grid-column: 2/4;
     max-width: 300px;
   }
   #text-meme-content #text-area {
-    grid-column: 3/5;
+    grid-column: 4/6;
   }
   #text-meme-content #text-area input {
     width: 200px;
     height: 25px;
   }
-  #text-meme-content button {
-    text-align: center;
+  #control {
+    max-width: 750px;
     margin: 0 auto;
+    text-align: center;
   }
 </style>
